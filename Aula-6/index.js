ffc54dafa664def42ba445a21be4e366ae906afe
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-05-13 11:40:59 -0300

    Aula 6 variaveis

---
+++ Aula-6/index.js
@@ -0,0 +1,11 @@
+// Declaração
+
+var nome, sobreNome, nomeCompleto;
+
+// Atribuição de elementos
+
+nome = "Cledimilson"
+sobreNome = "Santos"
+nomeCompleto = nome + " " + sobreNome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
