b4028260fb1b441d6651ac881a23f4d02a6bff71
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-05-13 11:57:29 -0300

    Aula 7 aritmeticos

---
+++ Aula-7/index.js
@@ -0,0 +1,20 @@
+/* Vamos entender os operadores:
+--> Os operadores javaScript são usados para atribuir valores, comparar valores, executar operações arientimeticas e muito mais
+
+-------------------------------------
+
+-->> São os sinais que usamos:
++ , - , *, /, =, ++, --, +=, &&, ||, Etc...
+
+========================================
+
+-->> São separados em 6 'Categorias':
+
+1) Operadores Aritméticos (Matematicos)
+2) Operadores de Atribuição
+3) Operadores de Sequência
+4) Operadores de Comparação
+5) Operador Condicional (Ternário)
+6) Operador Lógicos
+
+*/
