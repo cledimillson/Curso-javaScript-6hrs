f4ee9ea695cac37086bb93195862e727c0679429
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-06-09 12:04:06 -0300

    projet

--- Lista de tarefa projeto min/js/controle.js
+++ Lista de tarefa projeto min/js/controle.js
@@ -1,2 +1,52 @@
+let contador = 0
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('areaLista');
+
+function addTarefa() {
+
+    // Pegar o valor digitado no input
+    let valorInput = input.value;
+
+    // Se não for vazio, nem nulo, nem indefinido
+    if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i class="mdi mdi-circle-outline"></i>
+
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+
+    </div>`;
+
+    // Adicionando novo item no campo
+    main.innerHTML += novoItem;
+
+    // Zerar os campos
+    input.value = ""
+    input.focus();
+    }
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id)
+
+input.addEventListener("keyup", function(event) {
+    // Se teclou ENTER (13)
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
+    }
+});
